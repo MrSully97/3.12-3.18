2aeccb6560eca0b4e1fd3d48951faa10cfa43655
Connor <csully710@gmail.com>
2025-09-18 13:08:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const Person = require("./models/Person");
+const app = express();
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [];
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${Date()}</p>
+        </div>`);
+})
+
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(person => {
+        response.json(person);
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(person => {
+        if(person) {
+            response.json(person);
+        }
+        else {
+            response.statusMessage = `No person at id: ${id}`;
+            response.status(404).end();
+        }
+    })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    console.log('check this body: ', body);
+
+    if(!body.name) {
+        return response.status(400).json({
+            error: "no name found"
+        })
+    }
+    else if(!body.number) {
+        return response.status(400).json({
+            error: "no number found"
+        })
+    }
+    // else if(persons.find(person => person.name === body.name)) {
+    //     return response.status(400).json({
+    //         error: "name must be unique"
+    //     })
+    // }
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson);
+    })
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}) 
---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length < 3) {
+    console.log('please provide all arguments');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://csully710_db_user:${password}@cluster0.84xqehp.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url);
+
+const phonebookSchema = new mongoose.Schema({
+        name: String,
+        number: String
+    });
+
+const Person = mongoose.model('Person', phonebookSchema);
+
+if(process.argv.length === 5) {
+    const name = process.argv[3];
+    const phone = process.argv[4];
+
+    const person = new Person({
+        name: name,
+        number: phone
+    });
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${phone} to phonebook`);
+        mongoose.connection.close();
+    });
+} 
+else if(process.argv.length === 3) {
+    Person.find({}).then(person => {
+        console.log(person);
+        mongoose.connection.close();
+    })
+}
